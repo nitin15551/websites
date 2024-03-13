104ab1d52c660e6aa20e717cfcf5e52eb6e1020b
nitin.kumar <nitin.kumar@otssolutions.com>
2024-03-13 19:36:12 +0530

    fileupdate

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Frontend\ContactusController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\ServicesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/',[HomeController::class,'index']);
+Route::get('/contact',[ContactusController::class,'index']);
+Route::get('/services',[ServicesController::class,'index']);
+Route::get('/blogDetails',[ServicesController::class,'blogindex']);
+
+
